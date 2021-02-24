90aea7747229485619fd19f516a1234bf43a8d48
Lionel Cheng <cheng@cerfacs.fr>
2021-02-24 15:19:24 +0100

    Cleaning of routines
    Use new implementation closer to the actual formulas in Hirsch

--- PDE/1D/sp_analysis.py
+++ PDE/1D/sp_analysis.py
@@ -5,53 +5,18 @@ import cmath
 import matplotlib.pyplot as plt
 from fd_schemes import bjs
 
-
-def G_LW(phi, sigma):
-    """ Return the Lax-Wendroff scheme amplification factor """
-    return 1 + sigma**2 * (np.cos(phi) - 1) - 1j * sigma * np.sin(phi)
-
-
-def G_FOU(phi, sigma):
-    """ Return the First Order Upwind scheme amplification factor """
-    return 1 - sigma * (1 - np.exp(-1j * phi))
-
-
-def G_SOU(phi, sigma):
-    """ Return the Second Order Upwind scheme amplification factor """
-    return (1 - sigma / 2 * (3 - 4 * np.exp(-1j * phi) + np.exp(-2j * phi))
-                + sigma**2 / 2 * (1 - 2 * np.exp(-1j * phi) + np.exp(-2j * phi)))
-
-def G_TOU(phi, sigma):
-    """ Return the Third Order Upwind scheme amplification factor -> unstable at low CFL """
-    return (1 - sigma / 6 * (11 - 18 * np.exp(-1j * phi) + 9 * np.exp(-2j * phi) - 2 * np.exp(-3j * phi))
-                    + sigma**2 / 2 * (2 - 5 * np.exp(-1j * phi) + 4 * np.exp(-2j * phi) - np.exp(-3j * phi)))
-
-def G_C1(phi, sigma):
-    return (0.5 * sigma * (sigma + 1) * np.exp(-1j * phi)
-                        + (1 - sigma**2)
-                        + 0.5 * sigma * (sigma - 1) * np.exp(1j * phi))
-
-def G_C2(phi, sigma):
-    """ Fourth order centered stable scheme """
-    return ((sigma - 1) * sigma * (sigma + 1) * (sigma + 2) / 24 * np.exp(-2j * phi)
-                    - (sigma - 2) * sigma * (sigma + 1) * (sigma + 2) / 6 * np.exp(-1j * phi)
-                    + (sigma - 2) * (sigma - 1) * (sigma + 1) * (sigma + 2) / 4
-                    - (sigma - 2) * (sigma - 1) * sigma * (sigma + 2) / 6 * np.exp(1j * phi)
-                    + (sigma - 2) * (sigma - 1) * sigma * (sigma + 1) / 24 * np.exp(2j * phi))
-
 def ampl_factor(phi, sigma, scheme):
     """ Return the amplification factor for a scheme with ju upwind points
     jd downwind points and coeffs coefficients """
     coeffs, ju, jd = bjs(sigma, scheme)
-    G = np.zeros_like(phi)
+    G = np.zeros_like(phi, dtype=np.complex128)
     for j in range(-ju, jd + 1):
         G += coeffs[ju + j] * np.exp(j * 1j * phi)
     return G
 
-def errors(G, phi, sigma):
+def errors(G_num, phi, sigma):
     """ Computation of diffusion and dispersion error for a constant advection
     speed problem """
-    G_num = G(phi, sigma)
     diff_err = abs(G_num)
     disp_err = np.zeros_like(phi)
     disp_err[0] = 1
